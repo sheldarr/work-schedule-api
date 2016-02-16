bb3bc031d5b11ac05abfbe6399fe99e329188deb
sheldar@wp.pl <sheldar@wp.pl>
2016-02-16 22:39:07 +0100

    Api method for creating workers

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.ext.cors import CORS
 
 import json
@@ -9,7 +9,7 @@ CORS(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "OK"
+    return 'OK', 200
 
 @app.route('/shift', methods=['GET'])
 def get_shifts():
@@ -23,24 +23,47 @@ def get_workers():
         workers = json.load(data_file)
         return jsonify(workers)
 
+@app.route('/worker', methods=['POST'])
+def create_worker():
+    if not request.json:
+        abort(400)
+
+    with open('./data/workers.json') as data_file:
+        data = json.load(data_file)
+        workers = data['workers']
+
+        worker = {
+            'id': max(worker['id'] for worker in workers) + 1,
+            'name': request.json['name'],
+            'schedule': []
+        }
+
+        workers.append(worker)
+        
+        open('./data/workers.json', 'w').write(
+            json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+        )
+
+        return "OK", 200
+
 @app.route('/worker/<int:worker_id>', methods=['DELETE'])
 def delete_worker(worker_id):
     with open('./data/workers.json') as data_file:
         data = json.load(data_file)
         workers = data["workers"]
-        print (workers)
+
         for i in range(len(workers)):
             if workers[i]["id"] == worker_id:
                 workers.pop(i)
                 break
 
         data["workers"] = workers
-        print(data)
 
         open("./data/workers.json", "w").write(
             json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
         )
-        return "OK"
+
+        return "OK", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
