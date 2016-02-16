48cc05191c35f5785922ef79e2ca40c4a32379e7
sheldar@wp.pl <sheldar@wp.pl>
2016-02-16 21:49:09 +0100

    Enabled cors

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 #!flask/bin/python
 from flask import Flask, jsonify
+from flask.ext.cors import CORS
+
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def index():
     return "OK"
 
 @app.route('/shift', methods=['GET'])
-def getShifts():
+def get_shifts():
     with open('./var/shifts.json') as data_file:
         shifts = json.load(data_file)
         return jsonify(shifts)
 
 @app.route('/worker', methods=['GET'])
-def getWorkers():
+def get_workers():
     with open('./var/workers.json') as data_file:
         workers = json.load(data_file)
         return jsonify(workers)
