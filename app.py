7a902de520bb41ec697ed09d358de59e78b925da
sheldar@wp.pl <sheldar@wp.pl>
2016-02-16 22:08:09 +0100

    Updated api

--- app.py
+++ app.py
@@ -13,15 +13,31 @@ def index():
 
 @app.route('/shift', methods=['GET'])
 def get_shifts():
-    with open('./var/shifts.json') as data_file:
+    with open('./data/shifts.json') as data_file:
         shifts = json.load(data_file)
         return jsonify(shifts)
 
 @app.route('/worker', methods=['GET'])
 def get_workers():
-    with open('./var/workers.json') as data_file:
+    with open('./data/workers.json') as data_file:
         workers = json.load(data_file)
         return jsonify(workers)
+
+@app.route('/worker/<int:worker_id>', methods=['DELETE'])
+def delete_worker(worker_id):
+    with open('./data/workers.json') as data_file:
+        data = json.load(data_file)
+        workers = data["workers"]
+        print (workers)
+        for i in range(len(workers)):
+            if workers[i]["id"] == worker_id:
+                workers.pop(i)
+                break
+        print(workers)
+        open("./data/workers_test.json", "w").write(
+            json.dumps(workers, sort_keys=True, indent=4, separators=(',', ': '))
+        )
+        return "OK"
 
 if __name__ == '__main__':
     app.run(debug=True)
